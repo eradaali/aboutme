5684a3b4290000ab20544ad4dbb03e6777122d2a
eradaali <erada.abdalrhman@gmail.com>
2021-04-26 22:49:46 +0300

    addfile

---
+++ js/app.js
@@ -0,0 +1,26 @@
+alert("welcome to my page please answer these yes/no Q and Enjoy getting to know me");
+ let Q1 = prompt("1.  Do you think that  the field that i studied is interesting ?");
+ Q1.toUpperCase();
+ //console.log('my studies are computer science'+Q1);
+alert ("iam intrest to your openion thx");
+  let Q2= prompt("2. my age is older than 22");
+  Q2.toUpperCase();
+ // console.log(' iam 22'+Q2);
+alert("my age is 22 :)");
+ let Q3 =prompt("3.Do you think that i had more than one hobbies in my childhood")
+ Q3.toUpperCase();
+// console.log('i loved these talents at that time'+Q3);
+alert("i had many hobbies in my childhood");
+ let Q4 = prompt("4.is my graduation project name emergency?");
+ Q4.toUpperCase();
+ //console.log('wewe the name of my project graduation'+Q4);
+alert("wewe the name of my project graduation and that is a funny name ");
+let Q5 = prompt("Did get a high score in her project");
+Q5.toUpperCase();
+//console.log('i worked very hard on my project '+Q5);
+alert("i am proud to receive a grade A");
+
+alert("thanks four your intrest to know me now let me to know your name :) ");
+ let nameU =prompt("what is yor name");
+alert(" nice name "+ nameU);
+alert("i realy thankful " +nameU +" good Buy ");
