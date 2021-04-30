74b673250bee09e71eeb044d611b4a859f4d0a05
eradaali <erada.abdalrhman@gmail.com>
2021-05-01 00:46:23 +0300

    chnew

--- js/app.js
+++ js/app.js
@@ -1,145 +1,193 @@
 
 'use strict';
-let score =0;
+let score = 0;
+let answers=[];
 
+alert("welcome to my page please answer these yes/y/no/n Q and Enjoy getting to know me");
+function fun1() {
+ 
+  let Q1 = prompt('Is my name Erada abdalrhman?');
+  switch (Q1.toUpperCase()) {
+    case 'YES':
+    case 'Y':  
+      alert('yes , my name is Erada now you know my name');
+      answers.push(Q1);
+      score++;
 
+return true;
+    case 'NO':
+      alert('no ,it is');
+      score--;
 
- function fun1() {
-  alert("welcome to my page please answer these yes/no Q and Enjoy getting to know me");
-let Q1 = prompt('Is my name Erada abdalrhman?');
-switch (Q1.toUpperCase()) {
-  case 'YES':
-    alert('yes , my name is Erada now you know my name');
-  score++;
+return false;
+    default:
 
+      alert('plz ask me agin ');
+  }
+
+}
+for (let i = 0; i < 6; i++) {
+
+  if (fun1()) {
     break;
-  case 'NO':
-    alert('no ,it is');
-  score--;
-    
-    break;
-  default:
-    
-    alert('plz ask me agin');
+  };
+
+
 }
 
-} 
-fun1();
 
- function fun2() {
-  let Q2 = prompt('my age is older than 22');
-switch (Q2.toUpperCase()) {
-  case 'YES': 
-  alert('yes , my age is 22');
-  score++
-    
-    break;
-  case 'NO': 
-  alert('no ,it is 22');
-  score--;
-   
-    break;
-  default:
-    
-    alert('plz ask me agin');
+function fun2() {
+  let Q2 = prompt('my age is 22');
+  switch (Q2.toUpperCase()) {
+    case 'YES':
+    case 'Y':
+      alert('yes , my age is 22');
+      answers.push(Q2);
+      score++
+
+      return true;
+    case 'NO':
+    case 'N':
+      alert('no ,it is 22');
+      score--;
+
+      return false;
+    default:
+
+      alert('plz ask me agin');
+  }
 }
+for (let i = 0; i < 6; i++) {
+
+  if (fun2()) {
+    break;
+  };
+
+
 }
-fun2();
- function fun3() {
+function fun3() {
   let Q3 = prompt('Do you think that i had more than one hobbies in my childhood');
-switch (Q3.toUpperCase()) {
-  case 'YES':
-    alert('yes , I am had more than one ');
-  score++;
-    
-    break;
-  case 'NO': 
-  alert('no ,actully  I am had more hobbies in my childhood');
-  score--;
-    
-    break;
-  default:
-    
-    alert('plz ask me agin');
-}
+  switch (Q3.toUpperCase()) {
+    case 'YES':
+      case 'Y':
+      alert('yes , I am had more than one ');
+      answers.push(Q3);
+      score++;
+      return true;
+    case 'NO':
+    case 'N':
+      alert('no ,actully  I am had more hobbies in my childhood');
+      score--;
 
-  
+      return false;
+    default:
+
+      alert('plz ask me agin');
+  }
+
+
 }
-fun3();
+for (let i = 0; i < 6; i++) {
+
+  if (fun3()) {
+    break;
+  };
+
+
+}
 function fun4() {
   let Q4 = prompt('is my graduation project name wewe?');
-switch (Q4.toUpperCase()) {
-  case 'YES':
-    alert('yes , my project name is wewe ');
-  score++;
-    
-    break;
-  case 'NO': 
-  alert('no ,actully my project name is wewe');
-  score--;
-    
-    break;
-  default:
-    alert('plz ask me agin');
-}
-}
-fun4();
- function fun5() {
-  //Q5
-let Q5 = prompt('Did get a high score in her project?');
-switch (Q5.toUpperCase()) {
-  case 'YES':
-    alert('yes ,i got A ');
-  score++;
-    
-    break;
-  case 'NO': 
-  alert('no ,i got A');
-  score--;
-  
-    break;
-  default:
-    
-    alert('plz ask me agin');
-}
-
-}
-fun5();
-
-function fun6(){
-  const startTheQuestion = function (correctAnswer,questionText) {
-    for (let i = 1; i < 5; i++) {
-      let Q6 = parseInt(prompt(questionText));
-      if (Q6 == correctAnswer) {
-        alert("The answer is correct!")
-        return true;
-      } else {
-        if (Q6 < correctAnswer) {
-          alert("the number is more than")
-  
-        } else if (Q6 > correctAnswer) {
-          alert("the number is less than")
-  
-        }
-        score--;
-      }
-    }
-    alert("Try a gain")
-    return false;
-  
-  }
-  
-  const myArray=[{answer:4,questionText:"How many programming languages ​​have I tried to learn"},{answer:2,questionText:"Ranking a movie Small Axe: Lovers Rock in the Top Ten list is"}
-  ];
-  
-  for(i=0;i<myArray.length;i++){
-  
-    const isCorrect = startTheQuestion(myArray[i].answer,myArray[i].questionText);
-    if(isCorrect){
+  switch (Q4.toUpperCase()) {
+    case 'YES':
+    case 'Y': 
+      alert('yes , my project name is wewe ');
+      answers.push(Q4);
       score++;
+
+      return true;
+    case 'NO':
+    case 'N':  
+      alert('no ,actully my project name is wewe');
+      score--;
+
+      return false;
+    default:
+      alert('plz ask me agin');
+  }
+}
+for (let i = 0; i < 6; i++) {
+
+  if (fun4()) {
+    break;
+  };
+
+
+}
+function fun5() {
+  //Q5
+  let Q5 = prompt('Did get a high score in her project?');
+  switch (Q5.toUpperCase()) {
+    case 'YES':
+    case 'Y': 
+      alert('yes ,i got A ');
+      answers.push(Q5);
+      score++;
+
+      return true;
+    case 'NO':
+    case 'N':   
+      alert('no ,i got A');
+      score--;
+
+      return false;
+    default:
+
+      alert('plz ask me agin');
+  }
+
+}
+for (let i = 0; i < 6; i++) {
+
+  if (fun5()) {
+    break;
+  };
+
+
+}
+
+
+const startTheQuestion = function (correctAnswer, questionText) {
+  for (let i = 1; i < 6; i++) {
+    let Q6 = parseInt(prompt(questionText));
+    if (Q6 == correctAnswer) {
+      alert("The answer is correct!")
+      return true;
+    } else {
+      if (Q6 < correctAnswer) {
+        alert("the number is more than")
+
+      } else if (Q6 > correctAnswer) {
+        alert("the number is less than")
+
+      }
+      score--;
     }
-  
   }
+  alert("Try a gain")
+  return false;
+
+}
+
+const myArray = [{ answer: 4, questionText: "How many programming languages ​​have I tried to learn" }, { answer: 2, questionText: "Ranking a movie Small Axe: Lovers Rock in the Top Ten list is" }
+];
+
+for (let i = 0; i < myArray.length; i++) {
+
+  const isCorrect = startTheQuestion(myArray[i].answer, myArray[i].questionText);
+  if (isCorrect) {
+    score++;
   }
-  fun6();
-  alert ("Your score is "+score)
+
+}
+
+alert("Your score is " + score)
